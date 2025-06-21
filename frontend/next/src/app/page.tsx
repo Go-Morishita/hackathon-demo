cbd84b7f1fd5cd81309f0763737b4494b5f46fc3
Go-Morishita <go20040128@gmail.com>
2025-06-21 10:23:30 +0900

    update frontend

--- frontend/next/src/app/page.tsx
+++ frontend/next/src/app/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+
 
 export default function Home() {
+
+  const [response, setResponse] = useState();
+
+  const backendTest = async () => {
+    try {
+      const res = await axios.get('http://localhost/api/');
+      console.log(res.data);
+      setResponse(res.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
   return (
     <div>
       <p>Hello!!</p>
+      <button className="bg-amber-200 border p-2 m-2" onClick={backendTest}>BackendTest</button>
+      <p>Response: {response}</p>
     </div>
   );
 }
